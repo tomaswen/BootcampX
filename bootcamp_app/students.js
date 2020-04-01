3152bbbd4680a6062a434f2e634952ffa00cf2ed
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-01 20:34:15 +0000

    Parameterize the query to prevent SQL injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,15 +9,16 @@ const pool = new Pool({
 
 const argvArr = process.argv.slice(2);
 const cohortName = argvArr[0];
-const limitNumber = argvArr[1];
+const limitNumber = argvArr[1] || 5;
+const values = [`%${cohortName}%`, limitNumber]
 
 pool.query(`
 SELECT students.id, students.name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohortName}%'
-LIMIT ${limitNumber};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,16 +6,16 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+const values = [`${process.argv[2]}`]
 pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances 
 FROM teachers 
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
-ORDER BY teachers.name;`
-).then(res => {
+ORDER BY teachers.name;` , values)
+.then(res => {
    res.rows.forEach(teacher => {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
    })
